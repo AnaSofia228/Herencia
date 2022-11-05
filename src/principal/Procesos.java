56472556e5a94036a8f43c44e6027de14d18e811
Ana Sofia Ricardo Aguilar <anasofiaricardoaguilar@gmail.com>
2022-11-05 10:52:38 -0500

    Modificando procesos

--- src/principal/Procesos.java
+++ src/principal/Procesos.java
@@ -44,84 +44,25 @@ public class Procesos {
 			cod = Integer.parseInt(JOptionPane.showInputDialog(menu));
 			
 			switch (cod) {
-			case 1:
+			case 1:				
 				
-				
-				miBarco.setNombreVehiculo(JOptionPane.showInputDialog("Ingresa el nombre del vehiculo: "));
-				miBarco.setNombreAcuatico(JOptionPane.showInputDialog("Ingresa el nombre acuatico: "));
-				miBarco.setNombreBarco(JOptionPane.showInputDialog("Ingrese el nombre del barco: "));
-				
-				listaBarco.add("Vehiculo: " + miBarco.getNombreVehiculo() + "\n" + "Acuatico: " + miBarco.getNombreAcuatico() 
-				+ "\n" + "Barco: " + miBarco.getNombreBarco());
-				
-				System.out.println(listaBarco);			
-				System.out.println("Usando al barco " + miBarco.getNombreBarco() + " llamo al acuatico " + miBarco.getNombreAcuatico());
-				System.out.println("Usando al acuatico " + miBarco.getNombreAcuatico() + " llamo al vehiculo " + miBarco.getNombreVehiculo());
-				System.out.println(miBarco.getNombreBarco() + miBarco.transportar());
-				System.out.println(miBarco.getNombreBarco() + miBarco.prenderMotor());
-				System.out.println(miBarco.getNombreBarco() + miBarco.navegar());
-				
-				System.out.println();
+				registrarBarcos();
 				
 				break;
 				
 			case 2: 
-				
-				miVelero.setNombreVehiculo(JOptionPane.showInputDialog("Ingrese el nombre del vehiculo: "));		
-				miVelero.setNombreAcuatico(JOptionPane.showInputDialog("Ingrese el nombre acuatico: "));
-				miVelero.setNombreVelero(JOptionPane.showInputDialog("Ingrese el nombre del velero: "));
-				
-				listaVelero.add("Vehiculo: " + miVelero.getNombreVehiculo() + "\n" + "Acuatico: " + miVelero.getNombreAcuatico()
-				+ "\n" + "Velero: " + miVelero.getNombreVelero());
-				
-				System.out.println(listaVelero);
-				System.out.println("Usando al velero " + miVelero.getNombreVelero() + " llamo al acuatico " + miVelero.getNombreAcuatico());
-				System.out.println("Usando al acuatico " + miVelero.getNombreAcuatico() + " llamo al vehiculo " + miVelero.getNombreVehiculo());
-				System.out.println(miVelero.getNombreVelero() + miVelero.transportar());
-				System.out.println(miVelero.getNombreVelero() + miVelero.izarVelas());
-				System.out.println(miVelero.getNombreVelero() + miVelero.navegar());
-				
-				System.out.println();
-				
+				registrarVeleros();
 				break;
 				
 			case 3: 
 				
-				miAvion.setNombreVehiculo(JOptionPane.showInputDialog("Ingrese el nombre del vehiculo: "));
-				miAvion.setNombreAereo(JOptionPane.showInputDialog("Ingrese el nombre aereo: "));
-				miAvion.setNombreAvion(JOptionPane.showInputDialog("Ingrese el nombre del avion: "));
-				
-				listaAvion.add("Vehiculo: " + miAvion.getNombreVehiculo() + "\n" + "Acuatico: " + miAvion.getNombreAereo()
-				+ "\n" + "Avion: " + miAvion.getNombreAvion());
-				
-				System.out.println(listaAvion);
-				System.out.println("Usando al avion " + miAvion.getNombreAvion() + " llamo al aereo " + miAvion.getNombreAereo());
-				System.out.println("Usando al aereo " + miAvion.getNombreAereo() + " llamo al vehiculo " + miAvion.getNombreVehiculo());
-				System.out.println(miAvion.getNombreAvion() + miAvion.transportar());
-				System.out.println(miAvion.getNombreAvion() + miAvion.bajarTrenDeAterrizaje());
-				System.out.println(miAvion.getNombreAvion() + miAvion.volar());
-				
-				System.out.println();
+				registrarAviones();
 				
 				break;
 				
 			case 4:
 				
-				miHelicoptero.setNombreVehiculo(JOptionPane.showInputDialog("Ingrese el nombre del vehiculo: "));
-				miHelicoptero.setNombreAereo(JOptionPane.showInputDialog("Ingrese el nombre aereo: "));
-				miHelicoptero.setNombreHelicoptero(JOptionPane.showInputDialog("Ingrese el nombre del helicoptero: "));
-				
-				listaHelicoptero.add("Vehiculo: " + miHelicoptero.getNombreVehiculo() + "\n" + "Aereo: " + miHelicoptero.getNombreAereo() 
-				+ "\n" + "Helicoptero: " + miHelicoptero.getNombreHelicoptero());
-				
-				System.out.println(listaHelicoptero);
-				System.out.println("Usando al helicoptero " + miHelicoptero.getNombreHelicoptero() + " llamo al aereo " + miHelicoptero.getNombreAereo());
-				System.out.println("Usando al aereo " + miHelicoptero.getNombreAereo() + " llamo al vehiculo " + miHelicoptero.getNombreVehiculo());
-				System.out.println(miHelicoptero.getNombreHelicoptero() + miHelicoptero.transportar());
-				System.out.println(miHelicoptero.getNombreHelicoptero() + miHelicoptero.encenderHelices());
-				System.out.println(miHelicoptero.getNombreHelicoptero() + miHelicoptero.volar());
-				
-				System.out.println();
+				registrarHelicopteros();
 				
 				break;
 				
@@ -149,6 +90,87 @@ public class Procesos {
 		} while (cod != 6);
 		
 		
+		
+	}
+
+	private void registrarHelicopteros() {
+		
+		miHelicoptero.setNombreVehiculo(JOptionPane.showInputDialog("Ingrese el nombre del vehiculo: "));
+		miHelicoptero.setNombreAereo(JOptionPane.showInputDialog("Ingrese el nombre aereo: "));
+		miHelicoptero.setNombreHelicoptero(JOptionPane.showInputDialog("Ingrese el nombre del helicoptero: "));
+		
+		listaHelicoptero.add("Vehiculo: " + miHelicoptero.getNombreVehiculo() + "\n" + "Aereo: " + miHelicoptero.getNombreAereo() 
+		+ "\n" + "Helicoptero: " + miHelicoptero.getNombreHelicoptero());
+		
+		System.out.println(listaHelicoptero);
+		System.out.println("Usando al helicoptero " + miHelicoptero.getNombreHelicoptero() + " llamo al aereo " + miHelicoptero.getNombreAereo());
+		System.out.println("Usando al aereo " + miHelicoptero.getNombreAereo() + " llamo al vehiculo " + miHelicoptero.getNombreVehiculo());
+		System.out.println(miHelicoptero.getNombreHelicoptero() + miHelicoptero.transportar());
+		System.out.println(miHelicoptero.getNombreHelicoptero() + miHelicoptero.encenderHelices());
+		System.out.println(miHelicoptero.getNombreHelicoptero() + miHelicoptero.volar());
+		
+		System.out.println();
+		
+	}
+
+	private void registrarAviones() {
+		
+		miAvion.setNombreVehiculo(JOptionPane.showInputDialog("Ingrese el nombre del vehiculo: "));
+		miAvion.setNombreAereo(JOptionPane.showInputDialog("Ingrese el nombre aereo: "));
+		miAvion.setNombreAvion(JOptionPane.showInputDialog("Ingrese el nombre del avion: "));
+		
+		listaAvion.add("Vehiculo: " + miAvion.getNombreVehiculo() + "\n" + "Acuatico: " + miAvion.getNombreAereo()
+		+ "\n" + "Avion: " + miAvion.getNombreAvion());
+		
+		System.out.println(listaAvion);
+		System.out.println("Usando al avion " + miAvion.getNombreAvion() + " llamo al aereo " + miAvion.getNombreAereo());
+		System.out.println("Usando al aereo " + miAvion.getNombreAereo() + " llamo al vehiculo " + miAvion.getNombreVehiculo());
+		System.out.println(miAvion.getNombreAvion() + miAvion.transportar());
+		System.out.println(miAvion.getNombreAvion() + miAvion.bajarTrenDeAterrizaje());
+		System.out.println(miAvion.getNombreAvion() + miAvion.volar());
+		
+		System.out.println();
+		
+	}
+
+	private void registrarVeleros() {
+		
+		miVelero.setNombreVehiculo(JOptionPane.showInputDialog("Ingrese el nombre del vehiculo: "));		
+		miVelero.setNombreAcuatico(JOptionPane.showInputDialog("Ingrese el nombre acuatico: "));
+		miVelero.setNombreVelero(JOptionPane.showInputDialog("Ingrese el nombre del velero: "));
+		
+		listaVelero.add("Vehiculo: " + miVelero.getNombreVehiculo() + "\n" + "Acuatico: " + miVelero.getNombreAcuatico()
+		+ "\n" + "Velero: " + miVelero.getNombreVelero());
+		
+		System.out.println(listaVelero);
+		System.out.println("Usando al velero " + miVelero.getNombreVelero() + " llamo al acuatico " + miVelero.getNombreAcuatico());
+		System.out.println("Usando al acuatico " + miVelero.getNombreAcuatico() + " llamo al vehiculo " + miVelero.getNombreVehiculo());
+		System.out.println(miVelero.getNombreVelero() + miVelero.transportar());
+		System.out.println(miVelero.getNombreVelero() + miVelero.izarVelas());
+		System.out.println(miVelero.getNombreVelero() + miVelero.navegar());
+		
+		System.out.println();
+		
+		
+	}
+
+	private void registrarBarcos() {
+		
+		miBarco.setNombreVehiculo(JOptionPane.showInputDialog("Ingresa el nombre del vehiculo: "));
+		miBarco.setNombreAcuatico(JOptionPane.showInputDialog("Ingresa el nombre acuatico: "));
+		miBarco.setNombreBarco(JOptionPane.showInputDialog("Ingrese el nombre del barco: "));
+		
+		listaBarco.add("Vehiculo: " + miBarco.getNombreVehiculo() + "\n" + "Acuatico: " + miBarco.getNombreAcuatico() 
+		+ "\n" + "Barco: " + miBarco.getNombreBarco());
+		
+		System.out.println(listaBarco);			
+		System.out.println("Usando al barco " + miBarco.getNombreBarco() + " llamo al acuatico " + miBarco.getNombreAcuatico());
+		System.out.println("Usando al acuatico " + miBarco.getNombreAcuatico() + " llamo al vehiculo " + miBarco.getNombreVehiculo());
+		System.out.println(miBarco.getNombreBarco() + miBarco.transportar());
+		System.out.println(miBarco.getNombreBarco() + miBarco.prenderMotor());
+		System.out.println(miBarco.getNombreBarco() + miBarco.navegar());
+		
+		System.out.println();
 		
 	}
 
